a846b3d2c29770a38a20ec39191f8a72fea0cef0
EC2 Default User <ec2-user@ip-172-31-9-209.ap-northeast-2.compute.internal>
2025-10-28 09:57:56 +0000

    update

--- agentic_core/code/basic_agentic_core/call_agenticCore.py
+++ agentic_core/code/basic_agentic_core/call_agenticCore.py
@@ -1,15 +1,21 @@
 import json
 import boto3
 import asyncio
+from clean import load_deployment_info
+
+
 
 async def invoke_agent():
     agentcore_client = boto3.client('bedrock-agentcore', region_name='us-west-2')
+
+    deployment_info = load_deployment_info()
+
     
     loop = asyncio.get_event_loop()
     response = await loop.run_in_executor(
         None,
         lambda: agentcore_client.invoke_agent_runtime(
-            agentRuntimeArn="arn:aws:bedrock-agentcore:us-west-2:916634376326:runtime/basic_llm_second-iIBiK03SRV",
+            agentRuntimeArn=deployment_info["agent_arn"],
             qualifier="DEFAULT",
             payload=json.dumps({"input_data": "태양의 온도는 얼마인가요?"})
         )
