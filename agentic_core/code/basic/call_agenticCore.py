07aab209dc66e5131879ae5e933207d92f0cb495
EC2 Default User <ec2-user@ip-172-31-9-209.ap-northeast-2.compute.internal>
2025-10-28 04:28:11 +0000

    update

---
+++ agentic_core/code/basic/call_agenticCore.py
@@ -0,0 +1,25 @@
+import json
+import boto3
+import asyncio
+
+async def invoke_agent():
+    agentcore_client = boto3.client('bedrock-agentcore', region_name='us-west-2')
+    
+    loop = asyncio.get_event_loop()
+    response = await loop.run_in_executor(
+        None,
+        lambda: agentcore_client.invoke_agent_runtime(
+            agentRuntimeArn="arn:aws:bedrock-agentcore:us-west-2:916634376326:runtime/basic_llm_second-iIBiK03SRV",
+            qualifier="DEFAULT",
+            payload=json.dumps({"input_data": "태양의 온도는 얼마인가요?"})
+        )
+    )
+    
+    for line in response['response'].iter_lines():
+        if line and line.startswith(b'data: '):
+            data = json.loads(line[6:].decode('utf-8'))
+            if data['type'] == 'stream':
+                print(data['content'], end='')
+    print()
+
+asyncio.run(invoke_agent())
---
+++ agentic_core/setup/setup.sh
@@ -0,0 +1,16 @@
+sudo apt install openjdk-22-jdk -y
+pip install langchain
+pip install langchain-core
+pip install langchain-aws
+
+# OpenSearch client
+pip install opensearch-py
+
+# AWS SDK
+pip install boto3
+
+# Additional dependencies that might be needed
+pip install requests
+pip install urllib3
+pip install bedrock-agentcore
+pip install bedrock-agentcore-starter-toolkit
